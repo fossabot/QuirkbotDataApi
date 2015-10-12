241c89a2b04aacd8fbf38748fadc7b8d6836c69f
Paulo Barcelos <paulo@azucrina.org>
2015-10-12 10:04:51 +0200

    Updated email messages

--- src/api/services/EmailService.js
+++ src/api/services/EmailService.js
@@ -3,6 +3,7 @@ var mandrill = require('node-mandrill/mandrill')( process.env.MANDRILL_API_KEY |
 var defaultMessage = {
 	to: [{email: 'no-reply@quirkbot.com', name: 'Quirkbot CODE'}],
 	from_email: 'no-reply@quirkbot.com',
+	from_name: 'Quirkbot CODE',
 	subject: "Hello from Quirkbot CODE",
 	text: "Hello, I sent this message using the Quirkbot API."
 }
@@ -25,9 +26,9 @@ var sendConfirmation = function( user, cb ) {
 		type: "to"
 	}];
 
-	message.subject = "Quirkbot CODE verification";
+	message.subject = "Welcome to Quirkbot CODE";
 	message.text = "Hey "+user.nickname+"! Welcome to Quirkbot CODE! Please verify your account by entering the link: " + process.env.APP_CONFIRMATION_URL + user.id;
-	message.html = "<h1>Hey "+user.nickname+", welcome to Quirkbot CODE!</h1><p>Please verify your account by entering the link: " + process.env.APP_CONFIRMATION_URL + user.id + "</p>";
+	message.html = "<h2>Hey "+user.nickname+", welcome to Quirkbot CODE!</h2><p>Please verify your account by entering the link: " + process.env.APP_CONFIRMATION_URL + user.id + "</p>";
 
 	send( message, cb );
 }
@@ -38,12 +39,12 @@ var sendReset = function( user, request, cb ) {
 		email: user.email,
 		type: "to"
 	}];
-	message.subject = "Quirkbot CODE password reset";
-	message.text = "Access " + process.env.APP_RESET_URL + request.token + " to reset your password. If you did not request to reset your password, please ignore this email.";
-	message.html = "<h1>Do you want to reset your password?</h1>"+
-		"<p>We recently got a request to reset the password for the user <b>"+user.nickname+"</b>." +
-		"To procceed with the reset, please access "+process.env.APP_RESET_URL + request.token+" and follow the instructions.</p><br>"+
-		"<p>If you did not request to reset your password, please ignore this email.</p>";
+	message.subject = "Password reset";
+	message.text = "Access " + process.env.APP_RESET_URL + request.token + " to reset your password. If you did not request a password reset, please ignore this email.";
+	message.html = "<h2>Do you want to reset your password?</h2>"+
+		"<p>We got a request to reset the password for the user <b>"+user.nickname+"</b>.</p>" +
+		"<p>To procceed with the reset, please access "+process.env.APP_RESET_URL + request.token+" and follow the instructions.</p>"+
+		"<p>If you did not request a password reset, please ignore this email.</p>";
 
 	send( message, cb );
 }
