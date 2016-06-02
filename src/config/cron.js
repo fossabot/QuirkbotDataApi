b6559e4189ba1d93a0e40ffd754490917cc1148e
Murilo Polese <murilopolese@gmail.com>
2016-06-02 15:53:58 +0200

    Cron to clean also refresh tokens

--- src/config/cron.js
+++ src/config/cron.js
@@ -2,14 +2,28 @@ module.exports.cron = {
   cleanTokens: {
     schedule: '* * */1 * *',
     onTick: function () {
-      console.log( 'Cleaning tokens', new Date() );
+      console.log( 'Cleaning access tokens', new Date() );
 			AccessToken.destroy({
 				expires: {
 					"lessThan": new Date()
 				}
 			})
 			.then( function( err, res ) {
-				console.log( 'Clean result', err, res );
+				console.log( 'Cleaning access tokens', err, res );
+			})
+    }
+  }
+  cleanRefresh: {
+    schedule: '* * */1 * *',
+    onTick: function () {
+      console.log( 'Cleaning refresh tokens', new Date() );
+			RefreshToken.destroy({
+				expires: {
+					"lessThan": new Date()
+				}
+			})
+			.then( function( err, res ) {
+				console.log( 'Cleaning refresh tokens result', err, res );
 			})
     }
   }
