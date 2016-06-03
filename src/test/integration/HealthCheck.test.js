cc3f3408b0b7c0416782be0dca3428a4217f404e
Murilo Polese <murilopolese@gmail.com>
2016-06-03 12:25:03 +0200

    Finished health tests

--- src/test/integration/HealthCheck.test.js
+++ src/test/integration/HealthCheck.test.js
@@ -2,11 +2,41 @@ var request = require( 'supertest-as-promised' );
 
 describe( 'HealthCheck', function() {
 
-  it( 'Should pass health check', function ( done ) {
+  it( 'should return 200 on /', function ( done ) {
     request( sails.hooks.http.app )
-      .post( '/' )
+      .get( '/' )
       .expect( 200 )
 			.end( done )
   });
+
+  it( 'should return 404 on /client', function ( done ) {
+    request( sails.hooks.http.app )
+      .get( '/client' )
+      .expect( 404 )
+			.end( done )
+  });
+
+  it( 'should return 404 on /accessToken', function ( done ) {
+    request( sails.hooks.http.app )
+      .get( '/accessToken' )
+      .expect( 404 )
+			.end( done )
+  });
+
+  it( 'should return 404 on /refreshToken', function ( done ) {
+    request( sails.hooks.http.app )
+      .get( '/refreshToken' )
+      .expect( 404 )
+			.end( done )
+  });
+
+  it( 'should return 404 on /resetRequest', function ( done ) {
+    request( sails.hooks.http.app )
+      .get( '/resetRequest' )
+      .expect( 404 )
+			.end( done )
+  });
+
+
 
 });
