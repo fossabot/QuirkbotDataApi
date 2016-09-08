7e5cc7108b51b45c5db7da224fa1db6cc2ac08a7
Paulo Barcelos <paulo@azucrina.org>
2016-09-08 19:49:24 +0200

    Exiting process in case parent in killed

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// In case this process is being spawned from a parent, make sure to exit
+// in case the parent is killed
+process.on('disconnect', function() {
+	process.exit();
+});
+
 /**
  * app.js
  *
